5fc4de9de4b7b9a2aee8f1ed0074a76a3ec0c69a
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-06-18 21:44:47 +0100

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = items.map((item) => item.category);
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
   const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
     const newItems = items.filter((item) => item.category === category);
     setMenuItems(newItems);
   };
